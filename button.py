6971d05ae1299fa019f4655746d1f43afe99f96f
YunnaArtemenko <yunna.artemenko@gmail.com>
2018-06-06 19:25:28 +0000

    code working without pygame

--- button.py
+++ button.py
@@ -4,6 +4,14 @@ button = Button(16)
 
 def button_pressed():
     button.wait_for_press
+    print ("True")
     return True
 button_pressed()
+
+def button_released():
+    button.wait_for_release
+    print ("True")
+    return True
+button_released()     
+    
 
